49562f26a87211754d6ecfc834a2fec01b9f8125
AnupBayaskar <112762548+AnupBayaskar@users.noreply.github.com>
2024-01-06 14:18:45 +0530

    revers
    let

---
+++ 07_reverseStringAssign.js
@@ -0,0 +1,15 @@
+function reverseString(str) {
+    console.log(`\nGiven string is :- "${str}"`);
+  let  result = "" ;
+    for (let i = str.length - 1; i >= 0; i--) {
+        let char = str.charAt(i)
+        if (char!=" ") {
+            result = result + char; 
+           
+        }
+    } console.log(result);
+}
+reverseString(`Hard work always plays back`)
+reverseString(`Soon I will be UI IT Champ`)
+
+
