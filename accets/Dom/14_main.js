7353eca64b074a365e9eb7d81c4a3236c2374895
AnupBayaskar <112762548+AnupBayaskar@users.noreply.github.com>
2024-01-24 14:28:04 +0530

    dom
    dom

---
+++ accets/Dom/14_main.js
@@ -0,0 +1,7 @@
+console.log(`************* Changing Heading color ************* `);
+const headingChange = document.querySelector('#myPrfl');
+headingChange.style.color="cadetblue";
+console.log(`************* Changing Project Description ************* `);
+let projectDis = document.querySelector("#prjctDscrption");
+projectDis.innerHTML='Project Description : This project is mainly used for student management in college, where It help to manage student record like student registration,student update information and to see all his or her acaemic acivities like score card,attendence';
+
